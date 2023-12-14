3dd4b4e943f3fbf18e2c5c29dca660b6c5d65f61
mutasim chaudhary <mutasim.chaud@gmail.com>
2023-12-14 15:23:17 +0000

    post a new note added

--- routes/notes.js
+++ routes/notes.js
@@ -10,6 +10,24 @@ notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
   
+notes.post('/', (req, res) => {
+  console.log(req.body);
 
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Tip added successfully`);
+  } else {
+    res.json('Error in adding tip');
+  }
+});
+  
 
 module.exports = notes;
